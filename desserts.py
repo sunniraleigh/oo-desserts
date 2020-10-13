9e2d8d0e9778ba3837e7ac2d8e309a4044c2b617
sunniraleigh <sraleigh@smith.edu>
2020-10-13 01:01:30 +0000

    add class method

--- desserts.py
+++ desserts.py
@@ -3,10 +3,15 @@
 
 class Cupcake:
     """A cupcake."""
-    
+     
+    cache = {}
+
+    def __repr__(self):
+        """Human-readable printout for debugging."""
+        return f'<Cupcake name="{self.name}" qty={self.qty}>'
 
     def __init__(self, name, flavor, price):
-        self.cache = {}
+        
         self.name = name
         self.flavor = flavor
         self.price = price
@@ -22,11 +27,24 @@ class Cupcake:
             print("Sorry, these cupcakes are sold out.")
         else:
             self.qty -= amount
+    
+    @staticmethod
+    def scale_recipe(ingredients, amount):
+        scale_ingredients = []
+        for item in ingredients:
+            new_amount = (item[0], item[1] * amount)
+            scale_ingredients.append(new_amount)
+        return scale_ingredients    
 
-
-    def __repr__(self):
-        """Human-readable printout for debugging."""
-        return f'<Cupcake name="{self.name}" qty={self.qty}>'
+    @classmethod   
+    def get(cls, name):
+        if name in cls.cache:
+            return cls.cashe[name]
+        else:
+            print("Sorry, that cupcake doesn\'t exist.")
+    
+ 
+ 
 
 
 if __name__ == '__main__':
